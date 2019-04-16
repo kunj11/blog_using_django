cca3ac3985c29f606392b24ed4a535aaed350529
DC <imkunj11@gmail.com>
2019-04-16 09:37:29 +0530

    models implementation

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User  # for 1 to many relationship
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    # Foe date_posted
+    #auto_now=True  => set time when the post was actually created
+    #auto_now_add  => set the time to only when this object is created, but can't update its value
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # on_delete-> when a user is deleted delete their post also
+
+    def __str__(self):
+        return self.title
